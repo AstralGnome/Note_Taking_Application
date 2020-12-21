27791565dfb414eeca1a510d026ebd5dbe20462c
Astral Gnome <reedkeenan@gmail.com>
2020-12-21 00:31:25 -0800

    got the pseudo code setup

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 // require fs module
 const fs = require("fs");
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,20 +12,58 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Common practice to register API routes before html routes
-app.get("/api/notes", function (req, res){
-  
+app.get("/api/notes", function (req, res) {
   //Use the fs module to read the file
   fs.readFile(__dirname, "db.json");
-  
+
   //THEN use .then and parse the file contents with JSON.parse() to get th real data
 
   //Send the parsed data back to the client with res.json()
 });
 
+//POST creates NEW THINGS on the server
+app.post("/api/notes", function (req, res) {
+  //Access POSTed data in req.body
+  req.body;
+
+  //Use the fs module to read the file
+  fs.readFile("db.json");
+
+  //THEN use .then and parse the file contents with JSON.parse() to get th real data
+
+  //Push the req.body to the array list
+
+  //JSON.stringify the array list back into a JSON string
+
+  //THEN save the contents back to the db.json file using the fs module
+
+});
+
+app.delete("/api/notes/:id", function(req, res){
+
+  //Access id from req.params.id for loop
+
+   //Use the fs module to read the file
+   fs.readFile("db.json");
+
+   //THEN use .then and parse the file contents with JSON.parse() to get th real data
+ 
+//Option A
+  //Find the matching index using the Array.findIndex() method
+  //Remove target elements using the Array.splice() method
+
+//Option B
+  //Use the Array.filter method to filter out the matching element
+  //Also this array is not defined. Use let.
+  myArray = myArray.filter(element => element.id !== req.params.id)
+
+  //Return any type of success message. Property of value assigned true??
+
+});
+
 app.get("/notes", function (req, res) {
   //Return contents of notes.html
   res.sendFile(path.join(__dirname, "notes.html" /** Reference 11.2 mini-project **/));
-  
 });
 
 // Star route should always come last
