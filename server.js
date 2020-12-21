3b16fa4e814c3c7e598e2f681d3c2fa775023027
Astral Gnome <reedkeenan@gmail.com>
2020-12-20 23:32:30 -0800

    added necessary app.get html routing to server.js

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
+app.get("/", function (req, res) {
+  //Return contents of index.html
+  res.sendFile(path.join(__dirname, "index.html") /** Reference 11.2 mini-project **/);
 });
 
-app.listen(PORT, function() {
+app.get("/notes", function (req, res) {
+  //Return contents of notes.html
+  res.sendFile(path.join(__dirname, "notes.html"));
+
+});
+
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
-}); 
+});
