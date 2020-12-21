10e745e66bad3dd3c0844b614a6db18ddec1bdbe
Astral Gnome <reedkeenan@gmail.com>
2020-12-20 23:21:19 -0800

    added all necesarry middleware and got the file ready to begin working through guideline necessitites

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'))
+
 app.get("/", function(req, res) {
-  res.send("Hello Wold");
+  res.send("Hello World");
 });
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
-});
+}); 
