8dca97f460d7ec61d867ac0499ad030c9aee693a
Astral Gnome <reedkeenan@gmail.com>
2020-12-20 22:48:11 -0800

    added node serv.js to start within script in package.json

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 var app = express();
-var PORT = 3000
+var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.send("Hello Wold");
 });
+
+"scripts": {
+  "start":"node server.js"
+}
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
